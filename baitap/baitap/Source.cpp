ecaa0a6d8250e3c6a422391a5224c96daa7f14eb
loc1005 <phuklok.1412@gmail.com>
2020-10-26 16:34:08 +0700

    congtru

---
+++ baitap/baitap/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << " nhap vao a va b:";
+	cin >> a >> b;
+	cout << endl;
+	cout << a << "+" << b << "=" << a + b<<endl;
+	cout << a << "-" << b << "=" << a - b << endl;
+	cout << a << "*" << b << "=" << a * b << endl;
+	cout << a << "/" << b << "=" << a / b << endl;
+	system("pause");
+	return 0;
+}
